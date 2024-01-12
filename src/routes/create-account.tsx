4ca782ae99e8089342ef0f345596b268ec05899c
wooyeol <dnduf158@gmail.com>
2024-01-12 18:00:07 +0900

    add: set error state

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -2,7 +2,8 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
 import styled from "styled-components";
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -17,6 +18,7 @@ const Title = styled.h1`
 `;
 const Form = styled.form`
   margin-top: 50px;
+  margin-bottom: 10px;
   display: flex;
   flex-direction: column;
   gap: 10px;
@@ -61,6 +63,7 @@ export default function CreateAccount() {
   };
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError("");
     if (isLoading || name === "" || email === "" || password === "") return;
     try {
       setLoading(true);
@@ -81,10 +84,12 @@ export default function CreateAccount() {
       navigate("/");
     } catch (e) {
       // setError
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
     } finally {
       setLoading(false);
     }
-    console.log(name, email, password);
   };
   return (
     <Wrapper>
