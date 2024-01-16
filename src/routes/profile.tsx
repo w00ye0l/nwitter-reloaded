a5c3b098594acb6db1226c5b78c74f27d547d00b
wooyeol <dnduf158@gmail.com>
2024-01-16 19:04:16 +0900

    add: user displayname edit

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { auth, db, storage } from "../firebase";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
 import {
@@ -40,8 +40,44 @@ const AvatarImg = styled.img`
 const AvatarInput = styled.input`
   display: none;
 `;
-const Name = styled.span`
+const Name = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
   font-size: 22px;
+`;
+const DisplaynameInput = styled.input`
+  border: 2px solid white;
+  padding: 0px 5px;
+  border-radius: 5px;
+  font-size: 18px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 18px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const EditImg = styled.div`
+  width: 24px;
+  height: 24px;
+  color: #1d9bf0;
+  cursor: pointer;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    svg {
+      opacity: 0.8;
+    }
+  }
 `;
 const Tweets = styled.div`
   display: flex;
@@ -54,6 +90,8 @@ export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
   const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [displayname, setDisplayname] = useState(user?.displayName ?? "");
+  const [editDisplayname, setEditDisplayname] = useState(false);
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (!user) return;
@@ -67,6 +105,20 @@ export default function Profile() {
         photoURL: avatarUrl,
       });
     }
+  };
+  const onEditChange = async () => {
+    if (!user) return;
+    if (!editDisplayname) {
+      setEditDisplayname(true);
+    } else {
+      await updateProfile(user, {
+        displayName: displayname,
+      });
+      setEditDisplayname(false);
+    }
+  };
+  const onDisplaynameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDisplayname(e.target.value);
   };
   const fetchTweets = async () => {
     const tweetQuery = query(
@@ -114,7 +166,42 @@ export default function Profile() {
         type="file"
         accept="image/*"
       />
-      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <Name>
+        {editDisplayname ? (
+          <DisplaynameInput
+            onChange={onDisplaynameChange}
+            placeholder={displayname}
+          />
+        ) : (
+          user?.displayName ?? "Anonymous"
+        )}
+        <EditImg onClick={onEditChange}>
+          {editDisplayname ? (
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+              <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+            </svg>
+          )}
+        </EditImg>
+      </Name>
       <Tweets>
         {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
